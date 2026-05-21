2769e9896952edea6b19d26d8f80d65b840011d8
ashishchoudhary999 <ashishchoudharyofficial0@gmail.com>
2026-05-21 21:00:00 +0530

    Upgraded student system to dictionary-based records

--- main.py
+++ main.py
@@ -2,18 +2,46 @@ students = []
 while True: 
  print("1. Add Student")
  print("2. View Students")
- print("3. Exit")
+ print("3. Search Student")
+ print("4. Exit")
 
- choice = int(input("Enter your choices: "))
-
+ try:
+  choice = int(input("Enter your choices: "))
+ except ValueError:
+   print("Please enter numbers only")
+   continue
+   
  if choice == 1:
-   student = input("Enter student name: ")
+   name = input("Enter name: ")
+   age = input("Enter age: ")
+   course = input("Enter course: ")
+
+   student = {
+     "name": name,
+     "age": age,
+     "course": course
+   }
+
    students.append(student)
    print("Students Added Successfully")
  elif choice == 2:
     for student in students:
       print(student)    
  elif choice == 3:
+   name = input("Enter Student name: ")
+
+   found = False
+
+   for student in students:
+     if student["name"] == name:
+       print("Student Found")
+       print(student)
+       found = True
+       break
+     
+     if not found:
+       print("Student Not Found") 
+ elif choice == 4:
     print("Exiting...")
     break
  else:
