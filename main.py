59379a3960950ebc8661f9b4bc009e759a826cbb
ashishchoudhary999 <ashishchoudharyofficial0@gmail.com>
2026-05-21 20:18:10 +0530

    Added student add and view features

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-print("1. Add Student")
-print("2. View Students")
-print("3. Exit")
+students = []
+while True: 
+ print("1. Add Student")
+ print("2. View Students")
+ print("3. Exit")
+
+ choice = int(input("Enter your choices: "))
+
+ if choice == 1:
+   student = input("Enter student name: ")
+   students.append(student)
+   print("Students Added Successfully")
+ elif choice == 2:
+    for student in students:
+      print(student)    
+ elif choice == 3:
+    print("Exiting...")
+    break
+ else:
+    print("Invalid choice")
+    
